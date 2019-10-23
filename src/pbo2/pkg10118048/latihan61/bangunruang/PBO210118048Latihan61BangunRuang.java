a010d590cd07629946ae2dc52b4bda98104484ff
nikoyama <nikoyama32@gmail.com>
2019-10-23 20:07:49 +0700

    tambah formatting

--- src/pbo2/pkg10118048/latihan61/bangunruang/PBO210118048Latihan61BangunRuang.java
+++ src/pbo2/pkg10118048/latihan61/bangunruang/PBO210118048Latihan61BangunRuang.java
@@ -23,20 +23,20 @@ public class PBO210118048Latihan61BangunRuang {
         Kerucut kerucut = new Kerucut();
         
         bola.setR(7);
-        System.out.println("Jari-jari Bola = " + bola.getR());
-        System.out.printf("Volume Bola = %.2f %n%n" , bola.hitungVolume());
+        System.out.println("Jari-jari Bola = " + bola.getR() + " cm");
+        System.out.printf("Volume Bola = %.2f cm\u00B3%n%n" , bola.hitungVolume());
         
         tabung.setR(10);
         tabung.setT(21);
-        System.out.println("Jari-jari Tabung = " + tabung.getR());
-        System.out.println("Tinggi Tabung = " + tabung.getT());
-        System.out.printf("Volume Tabung = %.2f %n%n" , tabung.hitungVolume());
+        System.out.println("Jari-jari Tabung = " + tabung.getR() + " cm");
+        System.out.println("Tinggi Tabung = " + tabung.getT() + " cm");
+        System.out.printf("Volume Tabung = %.2f cm\u00B3%n%n" , tabung.hitungVolume());
         
         kerucut.setR(9);
         kerucut.setT(14);
-        System.out.println("Jari-jari Kerucut = " + kerucut.getR());
-        System.out.println("Tinggi Kerucut = " + kerucut.getT());
-        System.out.printf("Volume Kerucut = %.2f %n" , kerucut.hitungVolume());
+        System.out.println("Jari-jari Kerucut = " + kerucut.getR() + " cm");
+        System.out.println("Tinggi Kerucut = " + kerucut.getT() + " cm");
+        System.out.printf("Volume Kerucut = %.2f cm\u00B3%n" , kerucut.hitungVolume());
     }
 
 }
